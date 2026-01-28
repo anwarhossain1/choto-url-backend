3ac81dda669dc481c696775e6b0824417690b6e7
anwarhossain1 <irishkhan33@gmail.com>
2026-01-28 12:09:09 +0600

    feat(link): schema add

---
+++ src/domains/links/schema.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const linkSchema = new mongoose.Schema(
+  {
+    ownerType: {
+      type: String,
+      enum: ["User", "Guest"],
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+    guestId: {
+      type: String,
+      default: null,
+    },
+    alias: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    longUrl: {
+      type: String,
+      required: true,
+    },
+    clicks: {
+      type: Number,
+      default: 0,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    expiresAt: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model("Link", linkSchema);
